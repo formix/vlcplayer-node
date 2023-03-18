7d78b307d527755ff37e0fef8dea119a02529135
formix <jeanphilippe.gravel@gmail.com>
2023-03-18 12:03:15 -0400

    rc works

--- test/VlcPlayerTests.mjs
+++ test/VlcPlayerTests.mjs
@@ -1,27 +1,27 @@
 import assert from "assert";
-import {VlcPlayer} from "../src/index.mjs";
+import {VlcPlayer} from "./../lib/index.mjs"
 
 
 describe("VlcPlayer", async () => {
     describe("#open()", async () => 
         it("Should start a new player with the given stream", async () => {
             let player = new VlcPlayer();
-            player.open("D:\\tests\\vlcplayer\\window-01-initial.mp4");
+            await player.open();
             assert.equal(true, player._vlc.pid > 0);
             await sleep(2000);
-            player.close();
+            await player.close();
         }));
 
-    describe("#play()", async () => 
-        it("Should start a new player whith a stream the play another one.", async () => {
-            let player = new VlcPlayer();
-            player.open("D:\\tests\\vlcplayer\\window-01-initial.mp4");
-            assert.equal(true, player._vlc.pid > 0);
-            await sleep(3000);
-            player.play("D:\\tests\\vlcplayer\\window-02-storm-begin.mp4");
-            await sleep(2000);
-            player.close();
-        }));
+    // describe("#play()", async () => 
+    //     it("Should start a new player whith a stream the play another one.", async () => {
+    //         let player = new VlcPlayer();
+    //         player.open("D:\\tests\\vlcplayer\\window-01-initial.mp4");
+    //         assert.equal(true, player._vlc.pid > 0);
+    //         await sleep(3000);
+    //         player.play("D:\\tests\\vlcplayer\\window-02-storm-begin.mp4");
+    //         await sleep(2000);
+    //         player.close();
+    //     }));
     }
 );
 
