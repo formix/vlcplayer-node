d53bf347f480c3ad0347b95d5eb9fe2c28d60860
formix <jeanphilippe.gravel@gmail.com>
2023-03-18 14:51:32 -0400

    implement add

--- test/VlcPlayerTests.mjs
+++ test/VlcPlayerTests.mjs
@@ -12,16 +12,16 @@ describe("VlcPlayer", async () => {
             await player.close();
         }));
 
-    // describe("#play()", async () => 
-    //     it("Should start a new player whith a stream the play another one.", async () => {
-    //         let player = new VlcPlayer();
-    //         player.open("D:\\tests\\vlcplayer\\window-01-initial.mp4");
-    //         assert.equal(true, player._vlc.pid > 0);
-    //         await sleep(3000);
-    //         player.play("D:\\tests\\vlcplayer\\window-02-storm-begin.mp4");
-    //         await sleep(2000);
-    //         player.close();
-    //     }));
+    describe("#play()", async () => 
+        it("Should start a new player whith a stream the play another one.", async () => {
+            let player = new VlcPlayer();
+            player.open("D:\\tests\\vlcplayer\\window-01-initial.mp4");
+            assert.equal(true, player._vlc.pid > 0);
+            await sleep(3000);
+            player.play("D:\\tests\\vlcplayer\\window-02-storm-begin.mp4");
+            await sleep(2000);
+            player.close();
+        }));
     }
 );
 
