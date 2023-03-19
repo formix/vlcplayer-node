457afbe968c5c3f8d28e0ea3a611789feaaf732b
formix <jeanphilippe.gravel@gmail.com>
2023-03-19 14:30:37 -0400

    Final test on RPI4

--- test/IntegratedTests.mjs
+++ test/IntegratedTests.mjs
@@ -1,18 +1,18 @@
-import { VlcPlayer, sleep } from "vlcplayer"
+import { VlcPlayer, sleep } from "../lib/index.mjs";
 
-let mediasHome = "/var/medias/";
+let mediasHome = "/var/xcape/videos/";
 const VIDEOS = {
-    BadWeather: `${mediasHome}2019_sc02_067_v001_BUFFER1-1080.mp4`, // 20 seconds
-    Tornado: `${mediasHome}2019_sc02_067_v001_TRIGGER1-1080.mp4`,   // 20 seconds
-    Flying: `${mediasHome}2019_sc02_067_v001_BUFFER2-1080.mp4`,     // 20 seconds
-    FallDown: `${mediasHome}2019_sc02_067_v001_TRIGGER2-1080.mp4`,  // 13 seconds
-    CropField: `${mediasHome}2019_sc02_067_v001_BUFFER3-1080.mp4`   // 20 seconds
+    BadWeather: `${mediasHome}Wizard-of-Oz-Tornado-2019_sc02_067_v001_BUFFER1-1080.mp4`, // 20 seconds
+    Tornado: `${mediasHome}Wizard-of-Oz-Tornado-2019_sc02_067_v001_TRIGGER1-1080.mp4`,   // 20 seconds
+    Flying: `${mediasHome}Wizard-of-Oz-Tornado-2019_sc02_067_v001_BUFFER2-1080.mp4`,     // 20 seconds
+    FallDown: `${mediasHome}Wizard-of-Oz-Tornado-2019_sc02_067_v001_TRIGGER2-1080.mp4`,  // 13 seconds
+    CropField: `${mediasHome}Wizard-of-Oz-Tornado-2019_sc02_067_v001_BUFFER3-1080.mp4`   // 20 seconds
 };
 
 let player = new VlcPlayer();
 await player.open();
 for (let key in VIDEOS) {
-    await player.add(VIDEOS[key]);
+    await player.enqueue(VIDEOS[key]);
 }
 await player.play(VIDEOS.BadWeather);
 await player.repeat("on");
